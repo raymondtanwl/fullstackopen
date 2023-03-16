4ea159d864a70de28bbd3055aaa79f4ce4d27b90
Raymond Tan <raymondpcxpert@gmail.com>
2023-03-16 16:41:42 +0800

    Revert react-query for redux-anecdotes folder

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,13 +1,10 @@
-import axios from 'axios'
 import { useEffect } from 'react'
-import { useQuery } from 'react-query'
 import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
-import { initializeAnecdotes, setAnecdotes } from './reducers/anecdoteReducer'
-import { getAnecdotes } from './services/anecdoteService'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
@@ -15,21 +12,6 @@ const App = () => {
   useEffect(() => {
     dispatch(initializeAnecdotes())
   }, [dispatch])
-
-  const result = useQuery('anecdotes', getAnecdotes, {
-    refetchOnWindowFocus: false,
-    retry: 1
-  })
-  console.log('query res', result)
-
-  if (result.isLoading) {
-    return <div>loading data...</div>
-  }
-
-  if (result.isError) {
-    // return <span>Error: {result.error.message}</span>
-    return <span>anecdote service not available due to problems in server</span>
-  }
 
   return (
     <div>
--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,61 +1,28 @@
-import { useMutation, useQuery, useQueryClient } from "react-query"
 import { useDispatch, useSelector } from "react-redux"
 import { updateAnecdoteVote } from "../reducers/anecdoteReducer"
 import { setNotification } from "../reducers/notificationReducer"
-import { getAnecdotes, voteAnecdote } from "../services/anecdoteService"
 // import { voteAnecdote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
-  // // useSelector either searches for or selects data from the Redux store
-  // // const anecdotes = useSelector(state => state)
-  // const anecdotes = useSelector(state => {
-  //   // console.log('AnecdoteList useSelector', state)
-  //   if (state.filter === 'ALL') {
-  //     return state.anecdotes
-  //   } else {
-  //     return state.anecdotes.filter(a => a.content.toLowerCase().includes(state.filter))
-  //   }
-  // })
+  // useSelector either searches for or selects data from the Redux store
+  // const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    // console.log('AnecdoteList useSelector', state)
+    if (state.filter === 'ALL') {
+      return state.anecdotes
+    } else {
+      return state.anecdotes.filter(a => a.content.toLowerCase().includes(state.filter))
+    }
+  })
 
   // useDispatch provide access to the dispatch function of the Redux store in index.js
   const dispatch = useDispatch()
 
-  const queryClient = useQueryClient()
-
-  // mutations are typically used to create/update/delete data or perform server side-effects
-  const newAnecdoteMutation = useMutation(voteAnecdote, {
-    onSuccess: (votedAnecdote) => {
-      console.log('useMutation voteAnecdote onSuccess', votedAnecdote)
-      // the name/key should be same as the one defined in useQuery('anecdotes')
-      queryClient.invalidateQueries('anecdotes')
-      dispatch(setNotification(`you voted '${votedAnecdote.content}'`, 5))
-    }
-  })
-
-  let anecdotes = []
-  const result = useQuery('anecdotes', getAnecdotes, {
-    refetchOnWindowFocus: false,
-    retry: 1
-  })
-  // console.log('AnecdoteList useQuery result', result.data)
-
-  if (result.isError) {
-    // return <span>Error: {result.error.message}</span>
-    return <span>anecdote service not available due to problems in server</span>
-  }
-
-  if (result.isLoading) {
-    return <div>loading data...</div>
-  } else {
-    anecdotes = result.data
-  }
-
   const vote = (anecdote) => {
-    // // dispatch(voteAnecdote(id))
-    // // dispatch({ type: 'anecdotes/voteAnecdote', payload: anecdote.id })
-    // dispatch(updateAnecdoteVote(anecdote))
-    // dispatch(setNotification(`you voted '${anecdote.content}'`, 5))
-    newAnecdoteMutation.mutate(anecdote)
+    // dispatch(voteAnecdote(id))
+    // dispatch({ type: 'anecdotes/voteAnecdote', payload: anecdote.id })
+    dispatch(updateAnecdoteVote(anecdote))
+    dispatch(setNotification(`you voted '${anecdote.content}'`, 5))
   }
 
   const sortedByVotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
