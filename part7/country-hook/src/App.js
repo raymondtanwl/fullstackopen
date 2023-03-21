c17dce1e69c99b4c4eff4333ef85a373c264744a
Raymond Tan <raymondpcxpert@gmail.com>
2023-03-21 19:56:56 +0800

    Solution for exercise 7.7

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Country from './components/Country'
 import axios from 'axios'
 
 const useField = (type) => {
@@ -16,44 +17,65 @@ const useField = (type) => {
 }
 
 const useCountry = (name) => {
+  // console.log('useCountry hook', name)
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  const _buildCountryData = (isFound, data) => {
+    if (isFound && data) {
+      return {
+        found: true,
+        data: {
+          name: data.name.common,
+          capital: data.capital[0],
+          population: data.population,
+          flag: data.flags.png
+        }
+      }
+    } else {
+      return {
+        found: false
+      }
+    }
+  }
+
+  useEffect(() => {
+    // console.log('useCountry useEffect name', name)
+    if (name) {
+      axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`).then(response => {
+        const isFound = (response.data.length > 0)
+        const countryData = _buildCountryData(isFound, response.data[0])
+        setCountry(countryData)
+      }).catch((error) => {
+        console.log('Error calling API from restcountries.com', error)
+        const countryData = _buildCountryData(false)
+        setCountry(countryData)
+      })
+    }
+  }, [name])
+  // https://react.dev/reference/react/useEffect#parameters
+  // https://react.dev/reference/react/useEffect#examples-dependencies
+  // tl;dr:
+  // not passed in: runs after every single render
+  // empty array: only run after initial render
+  // array with dependency(ies): runs after initial render and after re-renders with changed dependencies
+  // in this case, when name state has changed, trigger the function to call api and update country state which
+  // trigger re-render of App with country details being rendered
 
   return country
 }
 
-const Country = ({ country }) => {
-  if (!country) {
-    return null
-  }
-
-  if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
-    </div>
-  )
-}
-
 const App = () => {
+  // console.log('App init')
   const nameInput = useField('text')
   const [name, setName] = useState('')
+  // when name state changes, trigger useCountry hook
   const country = useCountry(name)
 
   const fetch = (e) => {
     e.preventDefault()
+    // on submit, update the name state
     setName(nameInput.value)
+    // console.log('>>> onSubmit fetch', name)
   }
 
   return (
